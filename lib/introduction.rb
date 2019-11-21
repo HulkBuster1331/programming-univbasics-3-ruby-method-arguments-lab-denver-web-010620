53b88900cde36f9fea14b3bcb39dfec3432a5170
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-21 17:42:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name = "Hulk", language = "Asgaurdian")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end  
+
 
 
 
